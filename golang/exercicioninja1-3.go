4d0fe036e5e4cdeabf027b255f44d921a5e4717c
Willamy Ungary <willamy.ungary@gmail.com>
2026-03-25 12:09:25 -0300

    Update exercicioninja1-3.go

--- golang/exercicioninja1-3.go
+++ golang/exercicioninja1-3.go
@@ -2,11 +2,12 @@ package main
 
 	import "fmt"
 
+	var w float64 = 3.1416
 	var x int = 44
 	var y string = "Jesus Cristo"
 	var z bool = true
 
 func main() {
-	s :=fmt.Sprintf("%v\t%v\t%v\t", x, y, z)
+	s :=fmt.Sprintf("%v\t%v\t%v\t%v", w, x, y, z)
 	fmt.Println(s)
 }
